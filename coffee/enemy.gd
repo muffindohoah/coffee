66c1d85e048d9cb6bd630f4a6619deed92e06148
dawnvoid <voiding.voided@gmail.com>
2024-09-21 22:31:02 -0400

    simplify enemy collision

--- coffee/enemy.gd
+++ coffee/enemy.gd
@@ -10,15 +10,20 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	
 	if !targets.is_empty():
 		
 		velocity.x = move_toward(velocity.x, targets[0].position.x, SPEED)
-
+	
 	move_and_slide()
 
+func hit(damage: int) -> void:
+	die()
 
-func _on_detectionrange_area_entered(area: Area2D) -> void:
-	if area.get_parent().is_in_group("player"):
-		targets.append(area.get_parent())
+func die() -> void:
+	queue_free()
+
+func _on_detectionrange_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		targets.append(body)
